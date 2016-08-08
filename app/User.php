9a1566419288c62335b1623bc7adbc5c93f38c86
Khalig Novruzli <khalig.novruzli@gmail.com>
2016-08-08 13:49:01 +0300

    Admin added, not working

--- app/User.php
+++ app/User.php
@@ -23,4 +23,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    public function Role()
+    {
+
+        return $this->belongsTo('App\Role');
+
+    }
 }
---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,1 @@
+<h3>It's working</h3>

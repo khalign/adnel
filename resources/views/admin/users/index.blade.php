4becb7a32c563beaec421d182cecb67d2ca89cde
Khalig Novruzli <khalig.novruzli@gmail.com>
2016-08-09 14:31:57 +0300

    users persisted without image

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,38 @@
-<h3>It's working</h3>
+@extends('layouts.admin')
+
+@section('content')
+
+    <table class="table table-hover">
+        <thead>
+          <tr>
+              <th>User ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Status</th>
+              <th>Created</th>
+              <th>Updated</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        @if($users)
+            @foreach($users as $user)
+
+          <tr>
+            <td>{{$user->id}}</td>
+            <td>{{$user->name}}</td>
+            <td>{{$user->email}}</td>
+            <td>{{$user->role->name}}</td>
+            <td>{{$user->is_active == 1 ? 'Active' : 'Not active'}}</td>
+            <td>{{$user->created_at->diffForHumans()}}</td>
+            <td>{{$user->updated_at->diffForHumans()}}</td>
+          </tr>
+
+            @endforeach
+        @endif
+
+        </tbody>
+     </table>
+
+    @endsection
